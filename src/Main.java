73abeac494e0d029a62d8c9aaca642ab53f5cf57
Gamaliel Quiroz <ghamiboxbox@gmail.com>
2025-05-05 20:01:32 -0300

    v1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.print.attribute.standard.JobPrioritySupported;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		JOptionPane.showMessageDialog(null, "Hola");
+	}
+}
